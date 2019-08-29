568a7540138a68d6c3ef59b05853ce2425bf11d5
Ruel Tingling <rvtingling@yahoo.com>
2019-08-29 13:05:03 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,18 @@
-# Add your variables here
+first_number = 20
+second_number = 5
+
+def sum('first_number' :+ 'second_number')
+  puts "#{sum}"
+end
+
+def difference('first_number' :- 'second_number')
+  puts "#{difference}"
+end
+
+def product('first_number' :* 'second_number')
+  puts "#{product}"
+end
+
+def quotient('first_number' :/ 'second_number')
+  puts "#{quotient}"
+end
