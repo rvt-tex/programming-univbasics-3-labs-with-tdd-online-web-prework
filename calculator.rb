19b8ec4722a3c16ff5944d4f381bb0116b892a67
Ruel Tingling <rvtingling@yahoo.com>
2019-08-29 12:45:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,1 @@
-# Add your variables here
-def first_number(number1)
-  puts "#{number}"
-end
-
-def second_number(number2)
-  puts "#{number2}"
-end
-
-first_number("20")
-Second_number("5")
+first_
