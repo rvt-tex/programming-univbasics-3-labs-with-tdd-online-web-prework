686ca3461d45bea317300a74877f1eb675c2d883
Ruel Tingling <rvtingling@yahoo.com>
2019-08-29 13:01:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -8,3 +8,11 @@ end
 def difference('first_number' :- 'second_number')
   puts "#{difference}"
 end
+
+def product('first_number' :* 'second_number')
+  puts "#{product}"
+end
+
+def quotient('first_number' :/ 'second_number')
+  puts "#{quotient}"
+end
